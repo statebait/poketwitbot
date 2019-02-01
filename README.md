3fb3f008b25eacc0e3fc591471fef6c65f6930b1
Mohamed Shadab <shadabdragon@gmail.com>
2019-02-01 23:34:35 +0530

    Working Bot (Hopefully)

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-WIP
+# PokeTwitBot - A twitter bot
+
+## Description
+
+A Simple Twitter Bot built using the npm [twit](https://github.com/ttezel/twit) Twitter API Client and the [PokeAPI](https://pokeapi.co/). The bot tweets about a random pokemon everyday.
+
+## Installation
+
+**1. Ensure that Node.js and yarn have been installed**
+
+- [Node](https://nodejs.org/)
+- [Yarn](https://yarnpkg.com/)
+
+**2. Clone the repo**
+
+```bash
+git clone https://github.com/shadxx7/poketwitbot.git
+```
+
+**3. Install Dependencies**
+
+```bash
+cd poketwitbot # Go to the project root directory
+yarn
+```
+
+**4. Create Config File**
+
+In the project root directory create a file `config.js` which contains your twitter app's access keys and tokens. A typical config.js file should look like this:
+
+```javascript
+//config.js
+
+module.exports = {
+  consumer_key: '', // insert consumer key (API Key)
+  consumer_secret: '', // insert consumer secret (API Secret)
+  access_token: '', //  insert access token
+  access_token_secret: '' // insert access token secret
+};
+```
+
+**5. Creating the sentPokemon.json file**
+
+- Navigate into the directory where you have cloned the repository
+- Create a new file called 'sentPokemon.json' and copy the following into it:
+
+```json
+{
+  "idArray": []
+}
+```
+
+**4. Run the bot**
+
+```bash
+cd poketwitbot # Go to the project root directory, if not already
+yarn start
+```
--- index.js
+++ index.js
@@ -1,30 +1,92 @@
 var axios = require('axios');
+var fs = require('fs');
+var Chance = require('chance');
+var _ = require('lodash');
+var schedule = require('node-schedule');
 
-function firePokeTweet() {
-  // while (keepGoing) {
+var temp = [];
+
+function capSize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+async function getUniqueRandomNo(temp, count) {
+  var chance = new Chance();
+  var randomNo = chance.integer({ min: 1, max: 802 });
+  if (count === 802) {
+    return 'Max Limit Reached';
+  } else if (temp.includes(randomNo)) {
+    count++;
+    getUniqueRandomNo(temp);
+  } else {
+    temp.push(randomNo);
+    if (await checkUnique(randomNo)) {
+      return randomNo;
+    } else {
+      getUniqueRandomNo(temp);
+    }
+  }
+}
+
+function firePokeTweet(id) {
   var twit = require('twit');
   var config = require('./config.js');
   var twitter = new twit(config);
   axios
-    .get('https://pokeapi.co/api/v2/pokemon/ditto/')
+    .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(function(response) {
-      console.log(response.data);
-      // twitter.post(
-      //   'statuses/update',
-      //   {
-      //     status: content
-      //   },
-      //   function(err, data, response) {
-      //     if (err) {
-      //       console.log('Caught Error', err.stack);
-      //     }
-      //   }
-      // );
+      var data = response.data;
+      var content = `Today's Pokemon is ${capSize(
+        data.name
+      )}!\n\nHere is some info about it:\n\nTypes - ${_.map(
+        data.types,
+        item => {
+          return capSize(item.type.name);
+        }
+      )}\nHeight - ${data.height}\nWeight - ${data.weight}\nAbilities - ${_.map(
+        data.abilities,
+        item => {
+          return capSize(item.ability.name);
+        }
+      )}`;
+      console.log('Log: firePokeTweet -> content\n', content);
+      twitter.post(
+        'statuses/update',
+        {
+          status: content
+        },
+        function(err, data, response) {
+          if (err) {
+            console.log('Caught Error', err.stack);
+          }
+        }
+      );
     })
     .catch(function(error) {
       console.log(error);
     });
-  // }
 }
 
-firePokeTweet();
+async function checkUnique(id) {
+  var data = await fs.readFileSync('sentPokemon.json');
+  var jsonData = JSON.parse(data);
+  var arrayData = jsonData.idArray;
+  if (arrayData.includes(id)) {
+    return false;
+  } else {
+    var newData = { idArray: [...arrayData, id] };
+    var newJsonData = JSON.stringify(newData);
+    await fs.writeFileSync('sentPokemon.json', newJsonData);
+    return true;
+  }
+}
+
+async function main() {
+  var count = 0;
+  var pokemonID = await getUniqueRandomNo(temp, count);
+  firePokeTweet(pokemonID);
+}
+
+schedule.scheduleJob('* * */1 * *', () => {
+  main();
+});
