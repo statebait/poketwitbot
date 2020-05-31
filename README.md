a48bae3db2f9fc812a8eef499696efe841ee1d9d
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-05-31 18:58:07 +0530

    refactor: multiple changes
    removed lodash
    cleaned up printing of abilities & types
    separated out creating tweet content
    now a threaded tweet is posted with additional description
    general code cleanup

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A Simple Twitter Bot built using the [twit](https://github.com/ttezel/twit) Twit
 
 ## Installation & Usage
 
-> The `api` folder in this repo is for the serverless API setup using [Zeit(Vercel)](https://vercel.com/). It uses an [Airtable](https://airtable.com/invite/r/GPY1lsA0) config for storing which pokemons have been tweeted and [EasyCron](https://www.easycron.com/?ref=167187) to hit this API everyday.
+> The `api` folder in this repo is for the serverless API setup using [Zeit(Vercel)](https://vercel.com/). It uses an [Airtable](https://airtable.com/invite/r/GPY1lsA0) config for storing which pokemons have been tweeted and uses a cron job service like [EasyCron](https://www.easycron.com/?ref=167187) to hit this API everyday.
 
 **1. Ensure that Node.js and yarn have been installed**
 
@@ -31,6 +31,7 @@ yarn
 In the project root directory create a file called `.env` which contains your twitter app's access keys and tokens. The file should look like this:
 
 ```bash
+TWITTER_USERNAME=
 TWITTER_CONSUMER_KEY=
 TWITTER_CONSUMER_SECRET=
 TWITTER_ACCESS_TOKEN=
--- bot.js
+++ bot.js
@@ -1,50 +1,71 @@
 const axios = require("axios");
 const fs = require("fs");
 const twit = require("twit");
-const _ = require("lodash");
 const cheerio = require("cheerio");
+
+// Loading vars into the environment from .env
 require("dotenv").config();
 
-//Function to Captilize the first letter of a string
-const capSize = function (string) {
+//Function to Capitalize the first letter of a string
+const capSize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-//Function to get the data of the pokemon
-const getPokemonData = async function (id) {
-  let content, name;
-  await axios
-    .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((response) => {
-      const data = response.data;
-      name = data.name;
-      content = `Today's Pokemon is ${capSize(
-        data.name
-      )}!\n\nHere is some info about it:\n\nTypes - ${_.map(
-        data.types,
-        (item) => {
-          return capSize(item.type.name);
-        }
-      )}\nHeight - ${data.height}\nWeight - ${data.weight}\nAbilities - ${_.map(
-        data.abilities,
-        (item) => {
-          return capSize(item.ability.name);
-        }
-      )}`;
-    })
-    .catch((err) => console.error(err));
-
-  return { content, name };
+const createTweetData = ({
+  name,
+  types,
+  height,
+  weight,
+  abilities,
+  description,
+  path,
+}) => {
+  return {
+    firstTweetContent: `Today's Pokemon is ${capSize(name)}!\n\n${description}`,
+    secondTweetContent: `@${
+      process.env.TWITTER_USERNAME
+    } Here is some more info about it:\n\nTypes - ${types.reduce(
+      (acc, item, index) => {
+        if (index === types.length - 1) return acc + capSize(item.type.name);
+        return acc + capSize(item.type.name) + ", ";
+      },
+      ""
+    )}\nHeight - ${height * 10} cm\nWeight - ${
+      weight / 10
+    } kg\nAbilities - ${abilities.reduce((acc, item, index) => {
+      if (index === abilities.length - 1)
+        return acc + capSize(item.ability.name);
+      return acc + capSize(item.ability.name) + ", ";
+    }, "")}`,
+    imagePath: path,
+  };
 };
 
-//Function to get the pokemons image
-const getImageAndTweet = async function (stuff) {
-  await axios
-    .get(`https://www.pokemon.com/us/pokedex/${stuff.name}`)
+//Function to get the data of the pokemon from the poke api
+const getPokemonData = async function (id) {
+  return await axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(({ data }) => {
+      return {
+        name: data.name,
+        types: data.types,
+        height: data.height,
+        weight: data.weight,
+        abilities: data.abilities,
+      };
+    })
+    .catch((err) => console.error(err));
+};
+
+//Function to get the pokemon's image and description by scraping www.pokemon.com
+const getPokemonImageAndDescription = async function (pokemonName) {
+  return await axios
+    .get(`https://www.pokemon.com/us/pokedex/${pokemonName}`)
     .then(async (res) => {
       const $ = cheerio.load(res.data);
       const imageUrl = $(".profile-images").children("img").attr("src");
-      await axios
+      const description = cheerio.text($(".version-y"));
+      return await axios
         .get(imageUrl, {
           responseType: "arraybuffer",
         })
@@ -52,15 +73,15 @@ const getImageAndTweet = async function (stuff) {
           const data = new Buffer.from(response.data, "binary");
           return fs.writeFileSync("/tmp/temp.png", data);
         })
-        .then(async () => {
-          await fireTweet(stuff);
+        .then(() => {
+          return { description: description.trim(), path: "/tmp/temp.png" };
         })
         .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
 };
 
-const fireTweet = async function (stuff) {
+const fireTweet = async function (tweetData) {
   const twitter = new twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -68,14 +89,16 @@ const fireTweet = async function (stuff) {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   });
 
-  const b64content = fs.readFileSync("/tmp/temp.png", { encoding: "base64" });
+  const b64content = fs.readFileSync(tweetData.imagePath, {
+    encoding: "base64",
+  });
 
   try {
     const { data } = await twitter.post("media/upload", {
       media_data: b64content,
     });
     const mediaIdStr = data.media_id_string;
-    const altText = `Supposed to be an image of ${stuff.name}`;
+    const altText = `Supposed to be an image of ${tweetData.name}`;
     const meta_params = {
       media_id: mediaIdStr,
       alt_text: { text: altText },
@@ -83,10 +106,14 @@ const fireTweet = async function (stuff) {
     await twitter.post("media/metadata/create", meta_params);
 
     const params = {
-      status: stuff.content,
+      status: tweetData.firstTweetContent,
       media_ids: [mediaIdStr],
     };
-    await twitter.post("statuses/update", params);
+    const tweet = await twitter.post("statuses/update", params);
+    await twitter.post("statuses/update", {
+      status: tweetData.secondTweetContent,
+      in_reply_to_status_id: tweet.data.id_str,
+    });
   } catch (err) {
     return err;
   }
@@ -94,8 +121,12 @@ const fireTweet = async function (stuff) {
 
 async function main(id) {
   try {
-    const data = await getPokemonData(id);
-    await getImageAndTweet(data);
+    const pokemonData = await getPokemonData(id);
+    const tweetData = createTweetData({
+      ...pokemonData,
+      ...(await getPokemonImageAndDescription(pokemonData.name)),
+    });
+    await fireTweet(tweetData);
     return { success: true, message: "Successfully tweeted." };
   } catch (err) {
     return { error: true, message: "Something went wrong." };
